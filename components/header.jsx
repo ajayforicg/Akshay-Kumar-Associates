a1930fe5d569b1607d6b0b3f6cd08fb52161e241
lalukrish <lalukrishnavallil01@gmail.com>
2025-04-15 15:27:13 +0530

    fix

--- components/header.jsx
+++ components/header.jsx
@@ -3,68 +3,116 @@
 import React, { useEffect } from "react";
 
 const Header = () => {
-  useEffect(() => {
-    import("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, []);
+  // useEffect(() => {
+  //   import("bootstrap/dist/js/bootstrap.bundle.min.js");
+  // }, []);
 
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container">
-        <a className="navbar-brand fw-bold" href="#">Navbar</a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-3">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
+    <>
+      <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
+          <a
+            href="https://flowbite.com/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Flowbite
+            </span>
+          </a>
+          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Get started
+            </button>
+            <button
+              data-collapse-toggle="navbar-sticky"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-sticky"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
               >
-                Dropdown
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+          </div>
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded-sm 
+             hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 
+             md:p-0 md:dark:hover:text-blue-500 dark:text-white 
+             dark:hover:bg-gray-700 dark:hover:text-white 
+             md:dark:hover:bg-transparent dark:border-gray-700 
+             [text-decoration:none]"
+              >
+                About
               </a>
-              <ul className="dropdown-menu">
-                <li><a className="dropdown-item" href="#">Action</a></li>
-                <li><a className="dropdown-item" href="#">Another action</a></li>
-                <li><hr className="dropdown-divider" /></li>
-                <li><a className="dropdown-item" href="#">Something else here</a></li>
-              </ul>
             </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded-sm 
+                 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 
+                 md:p-0 md:dark:hover:text-blue-500 dark:text-white 
+                 dark:hover:bg-gray-700 dark:hover:text-white 
+                 md:dark:hover:bg-transparent dark:border-gray-700 
+                 no-underline hover:no-underline"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded-sm 
+                 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 
+                 md:p-0 md:dark:hover:text-blue-500 dark:text-white 
+                 dark:hover:bg-gray-700 dark:hover:text-white 
+                 md:dark:hover:bg-transparent dark:border-gray-700 
+                 no-underline hover:no-underline"
+              >
+                Services
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded-sm 
+                 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 
+                 md:p-0 md:dark:hover:text-blue-500 dark:text-white 
+                 dark:hover:bg-gray-700 dark:hover:text-white 
+                 md:dark:hover:bg-transparent dark:border-gray-700 
+                 no-underline hover:no-underline"
+              >
+                Contact
+              </a>
             </li>
           </ul>
-
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">Search</button>
-          </form>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
